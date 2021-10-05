81ab6f8bf9a1016e1a5ab3c6ce9e0c7bca07fd15
siddhanta tripathi <89008652+siddhanta28@users.noreply.github.com>
2021-10-05 11:41:17 +0530

    Delete brigade.js

--- brigade.js
+++
@@ -1,7 +0,0 @@
-const { events, Job } = require("brigadier");
-events.on("exec", () => {
-  var one = new Job("first-job");
-  one.image = "alpine:3.4";
-  one.tasks = ["Hello World"];
-  one.run();
-});
