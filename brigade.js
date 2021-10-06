b041d54e7b77677671f1282855f4f82292af9cd4
siddhanta tripathi <89008652+siddhanta28@users.noreply.github.com>
2021-10-06 17:19:01 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,13 +1,15 @@
 const { events, Job } = require("brigadier");
 events.on("exec", () => {
-  var one = new Job("first-job");
-  one.image = "alpine:3.4";
-  one.tasks = ["echo Hello World"];
-  var goodbye = new Job("goodbye", "alpine:3.4")
-  goodbye.tasks = [
-    "echo Goodbye",
-    "echo World"
+  var dockerBuild = new Job("docker-build")
+  dockerBuild.image = "docker:dind";
+
+  dockerBuild.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 30",
+    "cd /src/",
+    "docker build -t sdobhal369/brigade-test:20 ."
   ]
-  goodbye.run()
-  one.run();
+
+  dockerBuild.run();
 });
+
