bc067e59e1ab431fef444b2a1ecc2097edceafc1
siddhanta tripathi <89008652+siddhanta28@users.noreply.github.com>
2021-10-07 13:51:48 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,15 +1,22 @@
 const { events, Job } = require("brigadier");
-events.on("exec", () => {
-  var dockerBuild = new Job("docker-build")
+events.on("exec", (e,project) => {
+  var dockerBuild = new Job("docker-packaging");
   dockerBuild.image = "docker:dind";
-
+  dockerBuild.privileged = true;
+  dockerBuild.env = {
+    DOCKER_DRIVER: "overlay",
+    DOCKER_USER: project.secrets.dockerLogin,
+    DOCKER_PASS: project.secrets.dockerPass
+  }
+    
   dockerBuild.tasks = [
     "dockerd-entrypoint.sh &",
     "sleep 30",
-    "cd /src/",
-    "docker build -t siddhanta28/brigade-test:20 ."
-  ]
-
+    "cd /src",
+    "docker build -t siddhanta28/brigade-test:10 .",
+    "docker login -u $DOCKER_USER -p $DOCKER_PASS",
+    "docker push siddhanta28/brigade-test:50"
+  ];
+  
   dockerBuild.run();
 });
-
